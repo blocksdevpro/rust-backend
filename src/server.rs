7098e0fbf57cf303b7e8b1099a6cfa98f3911b46
BlocksDevPro <blocksdev.pro@gmail.com>
2026-03-23 23:10:36 +0530

    fix: merge users router with server

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,15 @@
 use axum::Router;
 use tower_http::trace::TraceLayer;
 
-use crate::{modules::auth::routes::routes as auth_routes, state::AppState};
+use crate::state::AppState;
+use crate::{
+    modules::auth::routes::routes as auth_routes, modules::users::routes::routes as users_routes,
+};
 
 pub fn create_server(state: AppState) -> Router {
     Router::new()
         .merge(auth_routes())
+        .merge(users_routes())
         .layer(TraceLayer::new_for_http())
         .with_state(state)
 }
